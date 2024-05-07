fc2769e7f6fa7a9c8f3ea56b18a08e87cbd4dd9e
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2024-05-07 06:04:49 -0500

    Update twosum_concept.c

--- C/1337code/twosum_concept.c
+++ C/1337code/twosum_concept.c
@@ -1,39 +1,36 @@
 #include <stdio.h>
 
-int main() {
-  int i;
-  int j;
-  int test;
-  int test2;
-  int target = 8;
-  int targeti = 0;
-  int targetj = 0;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+   
+    int i;
+    int j;
+    int para1;
+    int para2;
+    int para1_target = 0;
+    int para2_target = 0;
+    int answer[] = {0,0};
 
-  for (i = 0; i <= 5; i++) {
-   printf("\ni set to value: ");
-   printf("%d\n", i);
-   test = i;
-   printf("\nTest set to value: ");
-   printf("%d\n", test);
-    for (j=0; j <= 5; j++) {
-    printf("\nj set to value: ");
-    printf("%d", j);
-    test2 = (test + j);
-    printf("\ntest2 set to value: ");
-    printf("%d\n", test2);
-    if (test2 == target) {
-      targeti = i;
-      targetj = j;
-      printf("\n!!!i and j saved!!!\n");
-      }
+    for (int i=0; i<numsSize; i++) { /* Loops through every element of the array */
+        para1=nums[i];
+
+        for (j=(i+1); j<=numsSize; j++) {
+            para2=(para1+nums[j]);
+            if (para2==target) {
+                answer[0]=i;
+                answer[1]=j;
+                printf("%d\n", answer[0]);
+                printf("%d\n", answer[1]);
+            }
+        }
     }
-  }
-  
-  printf("%d", targeti);
-  printf(" + ");
-  printf("%d", targetj);
-  printf(" = ");
-  printf("%d", target);
+    
+    return 0;
+}
+
+int main() {
+  int nums1[]= {2, 7, 11, 15};
+  int target1 = 9;
+  twoSum(nums1, 4, target1, 0);
   
   return 0;
 }
