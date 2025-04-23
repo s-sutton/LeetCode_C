b17fbc3a06dfb8a3da2bab9329cbf6057f4725a8
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2025-04-23 15:50:34 -0500

    Update twosum_concept.c, warmer
    2/3 done, but woah, I get some kind of floating point error on the second one?
    
    Both integers in the array get set to -1094795586. I wonder what that's about?

--- challenges/twosum_concept.c
+++ challenges/twosum_concept.c
@@ -2,22 +2,23 @@
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int numsAmount = (sizeof(nums) / sizeof(nums[0]));
-    int i;
+        int i;
     int j;
     int para1;
     int para2;
     int para1_target = 0;
     int para2_target = 0;
-    int answer[] = {0,0};
+    int* answer = malloc(2 * sizeof(int));
+    *returnSize = 2;
 
     for (int i = 0; i < numsAmount; i++) { /* Loops through every element of the array */
         para1 = nums[i];
 
-        for (j = (i + 1); j <= numsAmount; j++) {
+        for (j = (i + 1); j < numsAmount; j++) {
             para2 = (para1 + nums[j]);
             if (para2 == target) {
-                answer[0] = nums[i];
-                answer[1] = nums[j];
+                answer[0] = i;
+                answer[1] = j;
                 printf("%d\n", answer[0]);
                 printf("%d\n", answer[1]);
             }
