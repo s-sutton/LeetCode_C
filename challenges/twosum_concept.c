6686339d91e8c980ce251238388a3829a980cb68
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2025-04-29 10:19:27 -0500

    Update twosum_concept.c, replaced custom variable numsAmount with called variable numsSize

--- challenges/twosum_concept.c
+++ challenges/twosum_concept.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    int numsAmount = (sizeof(nums) / sizeof(nums[0]));
-        int i;
+    int i;
     int j;
     int para1;
     int para2;
     int* answer = malloc(2 * sizeof(int));
     *returnSize = 2;
 
-    for (int i = 0; i < numsAmount; i++) { /* Loops through every element of the array */
+    for (int i = 0; i < numsSize; i++) { /* Loops through every element of the array */
         para1 = nums[i];
 
-        for (j = (i + 1); j < numsAmount; j++) {
+        for (j = (i + 1); j < numsSize; j++) {
             para2 = (para1 + nums[j]);
             if (para2 == target) {
                 answer[0] = i;
